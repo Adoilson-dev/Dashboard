c5288458d67708889c9e51fb76704a4551d9e4d7
Adoilson-dev <adoilson.dev@gmail.com>
2023-06-06 09:47:27 -0300

    conexão mysql

--- php/conexão.php
+++ php/conexão.php
@@ -1,1 +1,15 @@
-$_POST
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "consulta";
+$port = 3306;
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=". $dbname,$user,$pass);
+
+    echo "Conexão Com o banco de dados realizado com sucesso."
+} catch (PDOException $err){
+    echo "Erro: Não foi possivel realizar conexão com o banco de dados. Erro gerado" . $err->getMessage();
+}
