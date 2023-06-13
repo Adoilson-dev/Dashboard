d77d87162e69ce45a99a8536cba43e0a27a6d60b
Adoilson-dev <adoilson.dev@gmail.com>
2023-06-13 17:18:39 -0300

    update

--- js/graficos.js
+++ js/graficos.js
@@ -1,76 +1,50 @@
-// inserção de dados nos gráficos
-const data1 = {
-    labels: ['Dinheiro', 'C.Credito', 'C.Debito'],
-    datasets: [{
-        data: [60, 20, 20],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-    }]
-};
+// Setup
+const data = {
+  labels: ['Dinheiro', 'Pix', 'C.Credito', 'C.Debito', 'Boleto', 'A Prazo', 'Trnsf.Bancaria'],
+  datasets: [{
+    label: 'Vendas por Finalizadoras',
+    data: [1500, 2000, 3000, 600, 800, 454, 745],
 
-const data2 = {
-    labels: ['Masculino', 'Feminino', 'infantil'],
-    datasets: [{
-        data: [40, 40, 20],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-    }]
-};
-
-const data3 = {
-    labels: ['Custo', 'Lucro',],
-    datasets: [{
-        data: [40, 60,],
-        backgroundColor: ['#FF6384', '#36A2EB',]
-    }]
-};
-
-const data4 = {
-    labels: ['Vendedor 1', 'Vendedor 2', 'Vendedor 3'],
-    datasets: [{
-        data: [15, 30, 55],
-        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-    }]
+    borderWidth: 1,
+    cutout: '60%',
+  }]
 };
 
 
-// configurações para os gráficos 'doughnut'
-const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    legend: {
-        position: 'right',
-        labels: {
-            fontColor: '#333',
-            fontSize: 12,
-            padding: 10
+// config do grafico
+const config = {
+  type: 'doughnut',
+  data: data,
+  options: {
+    plugins: {
+      tooltip: {
+        enabled: false // Desabilita os tooltips
+      },
+      datalabels: {
+        align: 'center',
+        anchor: 'end',
+        align: 'center',
+
+        formatter: (value, context) => { // Função de formatação dos rótulos
+          const datapoints = context.chart.data.datasets[0].data;
+          function totalSum(total, datapoint) {
+            return total + datapoint;
+          }
+          const totalValue = datapoints.reduce(totalSum, 0); // Calcula a soma total dos valores
+          const percentageValue = ((value / totalValue) * 100).toFixed(1); // Calcula a porcentagem correspondente ao valor
+          const display = [`${value} R$`, `${percentageValue} %`]; // Formata o rótulo com o valor e a porcentagem
+          return display;
         }
-    }
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  },
+  plugins: [ChartDataLabels]
 };
 
-// Renderização dos gráficos
-const ctx1 = document.getElementById('chart1').getContext('2d');
-new Chart(ctx1, {
-    type: 'doughnut',
-    data: data1,
-    options: options
-});
 
-const ctx2 = document.getElementById('chart2').getContext('2d');
-new Chart(ctx2, {
-    type: 'doughnut',
-    data: data2,
-    options: options
-});
-
-const ctx3 = document.getElementById('chart3').getContext('2d');
-new Chart(ctx3, {
-    type: 'doughnut',
-    data: data3,
-    options: options
-});
-
-const ctx4 = document.getElementById('chart4').getContext('2d');
-new Chart(ctx4, {
-    type: 'doughnut',
-    data: data4,
-    options: options
-});
+const myChart = new Chart(
+  document.getElementById('chart1'),
+  config
+);
---
+++ teste.js
@@ -0,0 +1,46 @@
+// setup
+const data = {
+    labels: ['Dinheiro', 'Pix', 'C.Credito', 'C.Debito', 'Boleto', 'A Prazo', 'Trnsf.Bancaria'],
+    datasets: [{
+      label: 'Vendas por Finalizadoras',
+      data: [1500, 2000, 3000, 600, 800, 454, 745],
+      
+      borderWidth: 1,
+      cutout: '60%',
+    }]
+  };
+  
+  // config do grafico
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      plugins: {
+        tooltip: {
+          enabled: false // Desabilita os tooltips
+        },
+        datalabels: {
+          align: 'center',
+          formatter: (value, context) => { // Função de formatação dos rótulos
+            const datapoints = context.chart.data.datasets[0].data;
+            function totalSum(total, datapoint) {
+              return total + datapoint;
+            }
+            const totalValue = datapoints.reduce(totalSum, 0); // Calcula a soma total dos valores
+            const percentageValue = ((value / totalValue) * 100).toFixed(1); // Calcula a porcentagem correspondente ao valor
+            const display = [`${value}`, `${percentageValue}%`]; // Formata o rótulo com o valor e a porcentagem
+            return display;
+          }
+        }
+      },
+      responsive: true,
+      maintainAspectRatio: false
+    },
+    plugins: [ChartDataLabels]
+  };
+  
+
+  const myChart = new Chart(
+    document.getElementById('chart1'),
+    config
+  );
