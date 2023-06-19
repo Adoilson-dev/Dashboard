5abb516040cbb26a25bb973dc0f07e7af9a79178
Adoilson-dev <adoilson.dev@gmail.com>
2023-06-19 16:19:08 -0300

    update

--- index.php
+++ index.php
@@ -1,3 +1,62 @@
+<?php
+
+// variavel para Conexão com o banco de dados
+$host = '127.0.0.1';
+$dbName = 'sistemasit';
+$username = 'root';
+$password = '123456';
+$port = 3306;
+
+// Verificar se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Obter as datas do formulário
+    $dataInicial = $_POST['data-inicial'];
+    $dataFinal = $_POST['data-final'];
+
+    // Conexão com o banco de dados
+    $conexao = mysqli_connect($host, $username, $password, $dbName, $port);
+
+    // Verificar se ocorreram erros de conexão
+    if (mysqli_connect_errno()) {
+        die("Falha na conexão: " . mysqli_connect_error());
+    }
+
+    // Consulta SQL com busca por data
+    $query = "SELECT GROUP_CONCAT(fodesc) AS nome, GROUP_CONCAT(valor) AS valor
+        FROM (
+            SELECT sfeform.focodi, SUM(sfepaav.pavalor) AS valor
+            FROM sfepaav
+            JOIN sfeform ON sfepaav.padocum = sfeform.focodi
+            WHERE sfepaav.padata BETWEEN '$dataInicial' AND '$dataFinal'
+            GROUP BY sfeform.focodi
+        ) AS subquery
+        JOIN sfeform ON subquery.focodi = sfeform.focodi";
+
+    $resultado = mysqli_query($conexao, $query);
+
+    // Verificar erros de consulta
+    if (!$resultado) {
+        die("Erro na consulta: " . mysqli_error($conexao));
+    }
+
+    $dados = mysqli_fetch_array($resultado, MYSQLI_ASSOC);
+
+    $nomes = explode(',', $dados['nome']);
+    $valores = explode(',', $dados['valor']);
+
+    // Fechar a conexão com o banco de dados
+    mysqli_close($conexao);
+} else {
+    
+    // se o formulario retornar vazio atribuir dados inciais vazios
+    
+    $dataInicial = '';
+    $dataFinal = '';
+    $nomes = [];
+    $valores = [];
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -9,17 +68,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet' hrf="menu">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
-    <link rel="stylesheet" href="./css/menu.css">
-    <link rel="stylesheet" href="./css/footer.css">
-    <link rel="stylesheet" href="./css/seach-bar.css">
-    <link rel="stylesheet" href="./css/nav-bar.css">
-    <link rel="stylesheet" href="./css/charts.css">
-    <link rel="stylesheet" href="./css/preloader.css">
+    <script src="./assets/frameworks/cdn.jsdelivr.net_npm_chart.js@4.3.0_dist_chart.umd.min.js"></script>
+    <script src="./assets/frameworks/cdn.jsdelivr.net_npm_chartjs-plugin-datalabels@2.0.0"></script>
+    <link href='./assets/frameworks/unpkg.com_boxicons@2.1.4_css_boxicons.min.css' rel='stylesheet' hrf="menu">
+    <link rel="stylesheet" href="./assets/frameworks/cdnjs.cloudflare.com_ajax_libs_font-awesome_5.15.3_css_all.min.css">
+    <link rel="stylesheet" href="./assets/frameworks/cdnjs.cloudflare.com_ajax_libs_normalize_8.0.1_normalize.min.css">
+    <link rel="stylesheet" href="./assets/css/menu.css">
+    <link rel="stylesheet" href="./assets/css/footer.css">
+    <link rel="stylesheet" href="./assets/css/seach-bar.css">
+    <link rel="stylesheet" href="./assets/css/nav-bar.css">
+    <link rel="stylesheet" href="./assets/css/charts.css">
+    <link rel="stylesheet" href="./assets/css/preloader.css">
     <title>Sit web</title>
 
     <style>
@@ -78,19 +137,22 @@
             <div class="infor-usuario">
                 <label for="user"><i class="fas fa-address-card"></i> Usuario: Rubem jr</label>
             </div>
-
         </div>
-        <label class="data-legend" for="startDate">Data inicial:</label>
 
-        <input type="date" id="startDate" placeholder="Data Inicial">
+   
 
-        <label class="data-legend" for="endDate">Data final:</label>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <div>
+            <label class="data-legend" for="data-inicial">Data Inicial:</label>
+            <input type="date" id="data-inicial" name="data-inicial" value="<?php echo $dataInicial; ?>">
+            <label class="data-legend" for="data-final">Data Final:</label>
+            <input type="date" id="data-final" name="data-final" value="<?php echo $dataFinal; ?>">
+            <button type="submit">Buscar</button>
+        </div>
+    </form>
 
-        <input type="date" id="endDate" placeholder="Data Final">
+ </div>
 
-        <button onclick="animateButton()">Buscar</button>
-
-    </div>
 
     <div class="quadro">
 
@@ -195,14 +257,64 @@
         </div>
     </footer>
 
-    <script src="./js/botton.js"></script>
+
     <script src="./js/menu.js"></script>
     <script src="./js/preloader.js"></script>
-    <script src="./js/graficos/gr_finalizadoras.js"></script>
+    <!--<script src="./js/graficos/gr_finalizadoras.js"></script>
     <script src="./js/graficos/gr_categoria.js"></script>
     <script src="./js/graficos/gr_custo_lucro.js"></script>
     <script src="./js/graficos/gr_vendedor.js"></script>
-    <script src="./js/graficos/gr_comparatipo.js"></script>
+    <script src="./js/graficos/gr_comparatipo.js"></script>-->
 </body>
+
+<script>
+    const dataFinalizadoras = {
+      labels: [<?php echo "'" . implode("','", $nomes) . "'"; ?>],
+      datasets: [{
+        data: [<?php echo implode(',', $valores); ?>],
+        borderWidth: 1,
+        cutout: '60%',
+      }]
+    };
+
+    const configFinalizadoras = {
+      type: 'doughnut',
+      data: dataFinalizadoras,
+      options: {
+        plugins: {
+          tooltip: {
+            enabled: true // Desabilita os tooltips
+          },
+          datalabels: {
+            color: 'black',
+            anchor: 'center',
+            align:'end',
+            formatter: (value, context) => {
+              const labels = context.chart.data.labels; // Obtém as etiquetas das formas de pagamento
+              const datapoints = context.chart.data.datasets[0].data;
+
+              function totalSum(total, datapoint) {
+                return total + datapoint;
+              }
+
+              const totalValue = datapoints.reduce(totalSum, 0);
+              const percentageValue = ((value / totalValue) * 100).toFixed(1);
+              const label = labels[context.dataIndex]; // Obtém o nome da forma de pagamento correspondente ao rótulo atual
+              const display = [`${label}: R$${value} (${percentageValue}%)`]; // Adiciona o nome, valor e porcentagem ao rótulo
+              return display;
+            }
+          }
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      },
+      plugins: [ChartDataLabels]
+    };
+
+    const myChartFinalizadoras = new Chart(
+      document.getElementById('chart-finalizadoras'),
+      configFinalizadoras
+    );
+  </script>
 
 </html>
